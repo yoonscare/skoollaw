0e170573419722c2964d5bebe444a50cd367224b
yoonscare <136243845+yoonscare@users.noreply.github.com>
2024-11-17 15:14:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openGPTs(type) {
+    if (type === 'rules') {
+        window.open('https://chatgpt.com/g/g-nlqAPBsC4-hagcig-maenyueol-gaideu', '_blank');
+    } else if (type === 'grades') {
+        window.open('https://chatgpt.com/g/g-67358d2ad9a48190a8afe0070df68888-seongjeog-ceori-gyujeong-gaideu', '_blank');
+    }
+}
